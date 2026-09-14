bcb7b4a119931ca5f7ec96d94e9c5c2e4b4cf617
iananalistati-sketch <ian.analistati@gmail.com>
2026-09-14 17:59:14 -0300

    Integra provider e proteção de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-﻿import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import { AuthProvider } from './hooks/useAuth'
 import { MainLayout } from './layouts/MainLayout'
 import { DashboardPage } from './pages/DashboardPage'
 import { ModulePage } from './pages/ModulePage'
 import { LoginPage } from './pages/LoginPage'
 import { navigationGroups } from './lib/navigation'
 import './App.css'
+import './auth.css'
 
 export default function App() {
-  return <BrowserRouter><Routes>
+  return <AuthProvider><BrowserRouter><Routes>
     <Route path="/login" element={<LoginPage />} />
-    <Route element={<MainLayout />}>
-      <Route index element={<Navigate to="/dashboard" replace />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      {navigationGroups.flatMap(group => group.items).filter(item => item.path !== '/dashboard').map(item => <Route key={item.path} path={item.path} element={<ModulePage module={item} />} />)}
-      <Route path="*" element={<ModulePage />} />
+    <Route element={<ProtectedRoute />}>
+      <Route element={<MainLayout />}>
+        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        {navigationGroups.flatMap(group => group.items).filter(item => item.path !== '/dashboard').map(item => <Route key={item.path} path={item.path} element={<ModulePage module={item} />} />)}
+        <Route path="*" element={<ModulePage />} />
+      </Route>
     </Route>
-  </Routes></BrowserRouter>
+  </Routes></BrowserRouter></AuthProvider>
 }
